6b80ac909a3a317e78d5171538dcd1596df81a3d
Dustin Moore <dmoore@frostbytecafe.com>
2018-10-21 21:00:38 -0600

    Added terrain, ship, skybox, splash screen & music

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+    int currentScene;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        Invoke("LoadFirstLevel", 3.5f);
+	}
+
+    void LoadFirstLevel()
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene + 1, LoadSceneMode.Single);
+    }
+}
